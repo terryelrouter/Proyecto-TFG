bcbac590ac7b55d32a4f37b9912e898fe6d47f81
terryelrouter <terryelrouter@gmail.com>
2026-04-29 20:01:19 +0200

    Esqueleto Proyecto 1
    Es el esqueleto donde va a estar ubicado todo el contenido del proyecto

---
+++ PROYECTO TFG/demo/src/test/java/com/fullstack/proyecto/angular/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.proyecto.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
